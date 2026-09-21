96c7eadf72018feab051c94718498ae77a332d1e
Pranav Dhar Dwivedi <pranavdhardwivedi2006@gmail.com>
2026-09-21 05:46:20 +0000

    Fix event topic configuration in pipeline

--- src/aiops_pipeline.py
+++ src/aiops_pipeline.py
@@ -15,13 +15,13 @@ def run_pipeline(file_path):
     data = load_data(file_path)
 
     # INTENTIONAL ASSESSMENT ISSUE #2
-    producer_topic = EventTopic("service-events")
+    producer_topic = EventTopic("anomaly-events")
 
     detector = AnomalyDetector()
     producer = EventProducer(producer_topic)
 
     # INTENTIONAL ASSESSMENT ISSUE #3
-    consumer_topic = EventTopic("anomaly-events")
+    consumer_topic = producer_topic
     consumer = EventConsumer(consumer_topic)
 
     detected_events = []
